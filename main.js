100dcf7abf20f777163dcc3bb63edd2aa070b6fb
bhoomi221 <48841472+bhoomi221@users.noreply.github.com>
2022-01-28 18:40:33 -0500

    Add files via upload
    All files needed are here

---
+++ main.js
@@ -0,0 +1,133 @@
+var complete_notdoneSVG = '<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="512.000000pt" height="512.000000pt" viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet"> <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" fill="#000000" class="fill" stroke="none"> <path d="M2365 5114 c-362 -37 -633 -112 -930 -258 -261 -128 -447 -260 -659 -465 -218 -213 -377 -432 -512 -706 -126 -258 -203 -511 -246 -810 -17 -118 -17 -512 0 -630 43 -301 120 -553 247 -810 254 -515 653 -916 1160 -1165 197 -97 316 -142 510 -190 493 -124 989 -102 1475 65 141 48 421 188 550 274 306 203 577 480 774 791 26 41 80 141 120 221 493 991 287 2184 -510 2959 -210 205 -394 334 -659 466 -247 122 -495 199 -769 240 -97 14 -468 26 -551 18z m550 -343 c628 -106 1158 -449 1511 -977 179 -267 296 -573 351 -909 24 -153 24 -497 0 -650 -108 -668 -474 -1222 -1042 -1580 -243 -153 -537 -261 -850 -312 -154 -24 -497 -24 -650 1 -648 105 -1184 447 -1541 982 -179 267 -296 573 -351 909 -24 154 -24 497 0 650 81 497 290 908 637 1255 379 380 861 605 1400 654 108 10 418 -4 535 -23z"/> <path d="M3599 3441 c-24 -16 -352 -338 -729 -715 l-685 -686 -325 325 c-327 326 -362 355 -428 355 -44 0 -112 -41 -133 -80 -23 -42 -24 -111 -4 -154 23 -49 782 -802 827 -821 47 -19 79 -19 126 0 45 19 1553 1522 1577 1571 36 76 9 169 -60 209 -57 34 -109 33 -166 -4z"/> </g> </svg>'
+var remove_notdoneSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"> <g> <g> <path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3 c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9 C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7 c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2 c0.6,0,1.1,0.5,1.1,1.1V7z"/> </g> <g> <g> <path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/> </g> <g> <path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/> </g> <g> <path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8 C14.6,17.7,14.3,18,14,18z"/> </g> </g> </g> </svg>'
+var complete_doneSVG = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px" width="122.881px" height="122.88px" viewBox="0 0 122.881 122.88" enable-background="new 0 0 122.881 122.88" xml:space="preserve"><g><path class="fill" fill-rule="evenodd" clip-rule="evenodd" d="M61.44,0c33.933,0,61.441,27.507,61.441,61.439 c0,33.933-27.508,61.44-61.441,61.44C27.508,122.88,0,95.372,0,61.439C0,27.507,27.508,0,61.44,0L61.44,0z M34.106,67.678 l-0.015-0.014c-0.785-0.718-1.207-1.685-1.256-2.669c-0.049-0.982,0.275-1.985,0.984-2.777c0.01-0.011,0.019-0.021,0.029-0.031 c0.717-0.784,1.684-1.207,2.668-1.256c0.989-0.049,1.998,0.28,2.792,0.998l12.956,11.748l31.089-32.559v0 c0.74-0.776,1.723-1.18,2.719-1.204c0.992-0.025,1.994,0.329,2.771,1.067v0.001c0.777,0.739,1.18,1.724,1.205,2.718 c0.025,0.993-0.33,1.997-1.068,2.773L55.279,81.769c-0.023,0.024-0.048,0.047-0.073,0.067c-0.715,0.715-1.649,1.095-2.598,1.13 c-0.974,0.037-1.963-0.293-2.744-1L34.118,67.688L34.106,67.678L34.106,67.678L34.106,67.678z"/></g></svg>'
+
+var data = (localStorage.getItem('todolist')) ? JSON.parse(localStorage.getItem('todolist')): {
+    todo: [],
+    done: []
+}; 
+renderTodoList();
+
+//user clicked on the add button 
+document.getElementById('add').addEventListener('click', function() {
+    var value = document.getElementById('item').value;
+    if(value){
+        addItem(value);
+    }
+
+}); 
+
+document.getElementById('item').addEventListener('keydown', function(e){
+    var value = this.value; 
+    console.log(e.code);
+    if(e.code === "Enter" && value){
+        addItem(value); 
+    }
+});
+
+function addItem(value) {
+    addItemTodo(value);
+    data.todo.push(value);
+    updateObject(); 
+    document.getElementById('item').value= '';
+}
+
+function renderTodoList(){
+    if(!data.todo.length && !data.done.length){
+        return; 
+    } else {
+        for (var i =0; i< data.todo.length; i++){
+            var value = data.todo[i];
+            addItemTodo(value);
+
+        }
+        for (var j =0; j< data.done.length; j++){
+            var value = data.done[j];
+            addItemTodo(value, true);
+
+        }
+    }
+
+}
+
+
+function updateObject(){
+    localStorage.setItem('todolist', JSON.stringify(data));
+
+}
+
+function removeItem(){
+    //console.log(this.parentNode.parentNode);
+    var item = this.parentNode.parentNode; 
+    var parent = item.parentNode; 
+    var val = item.innerText;
+    var id = parent.id;
+
+    if(id === "done"){
+        data.done.splice(data.done.indexOf(val), 1); 
+    } else {
+        data.todo.splice(data.todo.indexOf(val), 1); 
+    }
+    updateObject();   
+    parent.removeChild(item);
+}
+
+
+function completeItem(){
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode; 
+    var val= item.innerText;
+
+    parent.removeChild(item); 
+
+    var ID= parent.id; 
+    var target = (ID === 'not-done') ? document.getElementById("done"):document.getElementById("not-done")
+    if(ID === "done"){
+        data.done.splice(data.done.indexOf(val), 1); 
+        data.todo.push(val);      
+    } else {
+        data.todo.splice(data.todo.indexOf(val), 1); 
+        data.done.push(val);
+    }
+    updateObject();
+
+    if(target === "done") {
+        target.insertBefore(item, target.childNodes[0]);
+    } else {
+        target.insertBefore(item, target.childNodes[0]);
+    }
+
+}
+
+function addItemTodo(activity, completed){
+    var list = (completed)?document.getElementById("done"):document.getElementById("not-done");
+    //console.log(list);
+
+    var item =  document.createElement('li'); 
+    item.innerText = activity; 
+
+    var buttons =  document.createElement('div'); 
+    buttons.classList.add('buttons'); 
+
+    var remove =  document.createElement('button'); 
+    remove.classList.add('remove'); 
+    remove.innerHTML = remove_notdoneSVG;
+
+    //Add click event for the remove button 
+    remove.addEventListener('click', removeItem); 
+    
+    var complete =  document.createElement('button'); 
+    complete.classList.add('complete'); 
+    complete.innerHTML = complete_notdoneSVG;
+
+    //add click event to complete the list 
+    complete.addEventListener('click', completeItem);
+
+
+    buttons.appendChild(remove); 
+    buttons.appendChild(complete);
+    item.appendChild(buttons);
+
+    list.insertBefore(item, list.childNodes[0]);
+}
+
